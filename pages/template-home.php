e52b4a5fb77fded5e47eff9569544e351e472867
eds4444 <efimov0204@gmail.com>
2021-12-11 09:55:32 +0200

    repiter

--- pages/template-home.php
+++ pages/template-home.php
@@ -4,43 +4,30 @@ Template Name: Home Template
 */
 get_header(); ?>
 
+<?php the_title();  ?>
+
+<?php the_content();  ?>
+
 <?php
 
-if( have_rows('flex_fields_template') ):
+if( have_rows('text') ): ?>
+ 
+        <ul>
+			<p>
 
-    while ( have_rows('flex_fields_template') ) : the_row();
+            <?php while( have_rows('text') ) : the_row();
 
-        if( get_row_layout() == 'content' ):
-            $c_text = get_sub_field('c_text');
-			echo $c_text;
-
-        elseif( get_row_layout() == 'list' ): 
-			if(have_rows('l_items')) : ?>
-					<ul>
-						<?php  while( have_rows('l_items') ) : the_row();
-								$l_i_item = get_sub_field('l_i_item'); ?>
-								<li><?php echo $l_i_item ?></li>
-						<?php endwhile; ?>
-					</ul>
-			<?php endif;
-
-		elseif( get_row_layout() == 'image' ): 
-			$im_item = get_sub_field('im_item');
-			echo wp_get_attachment_image( $im_item['ID'], array( $im_item['width'], $im_item['height'] ) );
-		elseif( get_row_layout() == 'gallery' ): 
-			if(have_rows('gallery_items')) : ?>
-				<div class="img">
-					<?php while(have_rows('gallery_items')) : the_row(); 
-							$image = get_sub_field('gallery_items_item');
-							echo wp_get_attachment_image( $image['ID'], array( $image['width'], $image['height'] ) );
-						endwhile; ?>
-				</div>
-			<?php endif;
-		endif;
-    endwhile;
-
-endif; 
-
-?>
+                $t_text = get_sub_field('t_text');
+				$t_text_area = get_sub_field('t_text_area');
+				$t_img = get_sub_field('t_img');
+				  echo $t_text;?><br>
+			      <?php echo wp_get_attachment_image( $t_img['ID'], array( $t_img['width'], $t_img['height'] ) );?><br>
+				  <?php echo $t_text_area; ?><br>
+            <?php endwhile; ?>
+			</p>
+		</ul>
+ <?php else :
+endif;
+ ?>
 
 <?php get_footer(); ?>
